49e74613c1b4c2972a545b5233f6cc66ab766359
merchjt <merch.jt@gmail.com>
2024-10-03 12:15:23 -0500

    Step 1
    Imported the required modules.
    Defined a function
    1. Display an intro/welcome message to the player. - done

---
+++ guessing_game.py
@@ -0,0 +1,16 @@
+"""
+Data Analysis Techdegree
+Project 1 - A Number Guessing Game
+--------------------------------
+"""
+
+# Imported random and statistics modules
+import random
+import statistics
+
+# Defined a function and updating. This will be function that starts and runs the game.
+def start_game():
+    # 1. Welcome message
+    print("WELCOME! I hope you enjoy the game!")
+    
+start_game()
