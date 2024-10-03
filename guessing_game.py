da092cdcec0bb3a82e7bf7b6c3dd60d0779ee82b
merchjt <merch.jt@gmail.com>
2024-10-03 12:26:27 -0500

    Step 2
    2. Store a random number as the answer/solution. - done

--- guessing_game.py
+++ guessing_game.py
@@ -12,5 +12,9 @@ import statistics
 def start_game():
     # 1. Welcome message
     print("WELCOME! I hope you enjoy the game!")
+    #2. Created a random number to guess using randrange(): https://docs.python.org/3/library/random.html#functions-for-integers
+    #2. Defined the range from 1 to 10. Noted that this function produces and int.
+    random_number = random.randrange(1,10) 
+    print(random_number) #DELETE - for testing purposes
     
 start_game()
